3dc85417b25300c5b333bb36f361daf1d1712dae
Sarah Freire <98456714+sarahfreire2710@users.noreply.github.com>
2025-04-09 05:16:02 +0200

    Update Suspended Clients 2025.sql

--- Suspended Clients 2025.sql
+++ Suspended Clients 2025.sql
@@ -1,6 +1,16 @@
-select CustId, CustCode, ClientCustDesc, SuspendReason, SuspendDate, PhysicalAddress, Suburb, SubArea, Longitude, Latitude
-from vw_SF_ClientDetails
-where SuspendMode = 'Y'
-and CustId > 1
-and SuspendDate >= '01 January 2025'
-order by SuspendDate
+SELECT 
+  CustId, 
+  CustCode, 
+  ClientCustDesc, 
+  SuspendReason, 
+  SuspendDate, 
+  PhysicalAddress, 
+  Suburb, 
+  SubArea,
+  Longitude,
+  Latitude
+FROM vw_SF_ClientDetails
+WHERE SuspendMode = 'Y'
+AND CustId > 1
+AND SuspendDate >= '01 January 2025'
+ORDER BY SuspendDate
